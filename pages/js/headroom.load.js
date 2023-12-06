afbfe7ec64eb8e1697354774f0bc867a6d84c6bf
User192940 <davfedchuk@gmail.com>
2023-12-05 18:39:15 -0600

    added AOS

--- pages/js/headroom.load.js
+++ pages/js/headroom.load.js
@@ -2,7 +2,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // select navigation element
     const elNavMain = document.querySelector(".mil-top-panel");
     // construct an instance of Headroom, passing the element
-    const headroom = new Headroom(elNavMain);
-    // initialise
-    headroom.init();
+    if(window.outerWidth <= 1200){
+        const headroom = new Headroom(elNavMain);
+        // initialise
+        headroom.init();
+    }
 });
